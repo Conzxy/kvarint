ea3e2333d6d0ce19cf6885edef3ab0efbc17f371
Conzxy <1967933844@qq.com>
2023-05-09 16:28:39 +0800

    opti(8bits): To 8bits encode & decode, use raw data

--- kvarint.c
+++ kvarint.c
@@ -4,52 +4,70 @@
 #include <assert.h>
 #include <stdio.h>
 
-#define DEF_ENCODE_FUNC_(bits_, num_t_, buf_t_)                                \
-  void kvarint_encode##bits_(num_t_ num, buf_t_ *buf)                          \
-  {                                                                            \
-    int idx = 0;                                                               \
-    while (num > 127) {                                                        \
-      buf->buf[idx++] = 0x80 | ((uint8_t)num & 0x7f);                          \
-      num >>= 7;                                                               \
-    }                                                                          \
-    buf->buf[idx] = num;                                                       \
-    buf->len = idx + 1;                                                        \
+#define DEF_ENCODE_FUNC_(bits_, num_t_, buf_t_)                                                    \
+  void kvarint_encode##bits_(num_t_ num, buf_t_ *buf)                                              \
+  {                                                                                                \
+    int idx = 0;                                                                                   \
+    while (num > 127) {                                                                            \
+      buf->buf[idx++] = 0x80 | ((uint8_t)num & 0x7f);                                              \
+      num             >>= 7;                                                                       \
+    }                                                                                              \
+    buf->buf[idx] = num;                                                                           \
+    buf->len      = idx + 1;                                                                       \
   }
 
-#define DEF_ENCODE_FUNC2_(bits_)                                               \
-  DEF_ENCODE_FUNC_(bits_, uint##bits_##_t, kvarint_buf##bits_##_t)
+#define DEF_ENCODE_FUNC2_(bits_) DEF_ENCODE_FUNC_(bits_, uint##bits_##_t, kvarint_buf##bits_##_t)
 
-DEF_ENCODE_FUNC2_(8)
+void kvarint_encode8(uint8_t num, kvarint_buf8_t *buf)
+{
+  buf->buf[0] = num;
+  buf->len    = 1;
+}
+
+// DEF_ENCODE_FUNC2_(8)
 DEF_ENCODE_FUNC2_(16)
 DEF_ENCODE_FUNC2_(32)
 DEF_ENCODE_FUNC2_(64)
 
-#define DEF_DECODE_FUNC_(bits_)                                                \
-  kvarint_errcode_en kvarint_decode##bits_(                                    \
-      void const *buf, size_t len, size_t *out_len, uint##bits_##_t *out)      \
-  {                                                                            \
-    assert(out);                                                               \
-    assert(out_len);                                                           \
-                                                                               \
-    int idx = 0;                                                               \
-    uint8_t const *buf8 = (uint8_t const *)buf;                                \
-    int shift = 0;                                                             \
-    *out = 0;                                                                  \
-    while (idx < len) {                                                        \
-      *out |= ((uint64_t)(buf8[idx] & 0x7f)) << shift;                         \
-      shift += 7;                                                              \
-      if (!(buf8[idx] & 0x80)) {                                               \
-        *out_len = idx + 1;                                                    \
-        return KVARINT_OK;                                                     \
-      }                                                                        \
-      if (shift > 63) return KVARINT_DECODE_BUF_INVALID;                       \
-      ++idx;                                                                   \
-    }                                                                          \
-                                                                               \
-    return KVARINT_DECODE_BUF_SHORT;                                           \
+#define DEF_DECODE_FUNC_(bits_)                                                                    \
+  kvarint_errcode_en                                                                               \
+      kvarint_decode##bits_(void const *buf, size_t len, size_t *out_len, uint##bits_##_t *out)    \
+  {                                                                                                \
+    assert(out);                                                                                   \
+    assert(out_len);                                                                               \
+                                                                                                   \
+    int            idx   = 0;                                                                      \
+    uint8_t const *buf8  = (uint8_t const *)buf;                                                   \
+    int            shift = 0;                                                                      \
+    *out                 = 0;                                                                      \
+    while (idx < len) {                                                                            \
+      *out  |= ((uint64_t)(buf8[idx] & 0x7f)) << shift;                                            \
+      shift += 7;                                                                                  \
+      if (!(buf8[idx] & 0x80)) {                                                                   \
+        *out_len = idx + 1;                                                                        \
+        return KVARINT_OK;                                                                         \
+      }                                                                                            \
+      if (shift > 63) return KVARINT_DECODE_BUF_INVALID;                                           \
+      ++idx;                                                                                       \
+    }                                                                                              \
+                                                                                                   \
+    return KVARINT_DECODE_BUF_SHORT;                                                               \
   }
+
+kvarint_errcode_en kvarint_decode8(void const *buf, size_t len, size_t *out_len, uint8_t *out)
+{
+  assert(out);
+  assert(out_len);
+  if (len < 1) {
+    return KVARINT_DECODE_BUF_SHORT;
+  }
+
+  *out_len = 1;
+  *out     = *(char const *)buf;
+  return KVARINT_OK;
+}
 
 DEF_DECODE_FUNC_(64)
 DEF_DECODE_FUNC_(32)
 DEF_DECODE_FUNC_(16)
-DEF_DECODE_FUNC_(8)
+// DEF_DECODE_FUNC_(8)
--- kvarint.h
+++ kvarint.h
@@ -18,32 +18,39 @@ typedef enum {
   KVARINT_DECODE_BUF_INVALID,
 } kvarint_errcode_en;
 
-#define KVARINT_DECL_DECODE_FUNC_(bits_)                                       \
-  kvarint_errcode_en kvarint_decode##bits_(                                    \
-      void const *buf, size_t buf_size, size_t *out_len, uint##bits_##_t *out)
+#define KVARINT_DECL_DECODE_FUNC_(bits_)                                                           \
+  kvarint_errcode_en kvarint_decode##bits_(                                                        \
+      void const      *buf,                                                                        \
+      size_t           buf_size,                                                                   \
+      size_t          *out_len,                                                                    \
+      uint##bits_##_t *out                                                                         \
+  )
 
 KVARINT_DECL_DECODE_FUNC_(64);
 KVARINT_DECL_DECODE_FUNC_(32);
 KVARINT_DECL_DECODE_FUNC_(16);
 KVARINT_DECL_DECODE_FUNC_(8);
 
-#define KVARINT_DECL_DECODE_FUNC_SIGNED_(bits_)                                \
-  INLINE kvarint_errcode_en kvarint_decode##bits_##s(                          \
-      void const *buf, size_t buf_size, size_t *out_len, int##bits_##_t *out)  \
-  {                                                                            \
-    /* Explain this to uint_*_t * to avoid right shift be a arithemetic shift  \
-     * that leave the most significant bit(MSB) no change.                     \
-     *                                                                         \
-     * since encode use left shift, it is a logic shift that fill the MSB with \
-     * 0, we must be also ensure the right shift operation be a logic shift.   \
-     */                                                                        \
-    uint##bits_##_t *p = (uint##bits_##_t *)out;                               \
-    const kvarint_errcode_en ret =                                             \
-        kvarint_decode##bits_(buf, buf_size, out_len, p);                      \
-                                                                               \
-    /* *p = (p is odd) ? (*p+1) / 2 : p / 2; */                                \
-    *p = (*p & 1) ? ~((*p - 1) >> 1) : (*p >> 1);                              \
-    return ret;                                                                \
+#define KVARINT_DECL_DECODE_FUNC_SIGNED_(bits_)                                                    \
+  INLINE kvarint_errcode_en kvarint_decode##bits_##s(                                              \
+      void const     *buf,                                                                         \
+      size_t          buf_size,                                                                    \
+      size_t         *out_len,                                                                     \
+      int##bits_##_t *out                                                                          \
+  )                                                                                                \
+  {                                                                                                \
+    /* Explain this to uint_*_t * to avoid right shift be a arithemetic shift                      \
+     * that leave the most significant bit(MSB) no change.                                         \
+     *                                                                                             \
+     * since encode use left shift, it is a logic shift that fill the MSB with                     \
+     * 0, we must be also ensure the right shift operation be a logic shift.                       \
+     */                                                                                            \
+    uint##bits_##_t         *p   = (uint##bits_##_t *)out;                                         \
+    const kvarint_errcode_en ret = kvarint_decode##bits_(buf, buf_size, out_len, p);               \
+                                                                                                   \
+    /* *p = (p is odd) ? (*p+1) / 2 : p / 2; */                                                    \
+    *p = (*p & 1) ? ~((*p - 1) >> 1) : (*p >> 1);                                                  \
+    return ret;                                                                                    \
   }
 
 KVARINT_DECL_DECODE_FUNC_SIGNED_(64)
@@ -51,18 +58,18 @@ KVARINT_DECL_DECODE_FUNC_SIGNED_(32)
 KVARINT_DECL_DECODE_FUNC_SIGNED_(16)
 KVARINT_DECL_DECODE_FUNC_SIGNED_(8)
 
-#define KVARINT_DEF_BUF_STRUCT_(bits_, max_bytes_)                             \
-  typedef struct {                                                             \
-    char buf[max_bytes_];                                                      \
-    size_t len;                                                                \
+#define KVARINT_DEF_BUF_STRUCT_(bits_, max_bytes_)                                                 \
+  typedef struct {                                                                                 \
+    char   buf[max_bytes_];                                                                        \
+    size_t len;                                                                                    \
   } kvarint_buf##bits_##_t
 
 KVARINT_DEF_BUF_STRUCT_(64, 10);
 KVARINT_DEF_BUF_STRUCT_(32, 5);
 KVARINT_DEF_BUF_STRUCT_(16, 3);
-KVARINT_DEF_BUF_STRUCT_(8, 2);
+KVARINT_DEF_BUF_STRUCT_(8, 1);
 
-#define KVARINT_DECL_ENCODE_FUNC_(bits_)                                       \
+#define KVARINT_DECL_ENCODE_FUNC_(bits_)                                                           \
   void kvarint_encode##bits_(uint##bits_##_t num, kvarint_buf##bits_##_t *buf)
 
 KVARINT_DECL_ENCODE_FUNC_(64);
@@ -70,30 +77,28 @@ KVARINT_DECL_ENCODE_FUNC_(32);
 KVARINT_DECL_ENCODE_FUNC_(16);
 KVARINT_DECL_ENCODE_FUNC_(8);
 
-#define KVARINT_DECL_ENCODE_FUNC_SIGNED_(bits_)                                \
-  INLINE void kvarint_encode##bits_##s(int##bits_##_t num,                     \
-                                       kvarint_buf##bits_##_t *buf)            \
-  {                                                                            \
-    /* Since -1 is (~0), ie. the maximum number of uint_*_t,                   \
-     * it will be encoded to 10 bytes of uint64_t.                             \
-     *                                                                         \
-     * However, user want the -1 also be encoded to 1 bytes like 1.            \
-     * For other negative whose absolute value is small, also be encoded to    \
-     * same bytes with their absolute.                                         \
-     *                                                                         \
-     * Algorithm:                                                              \
-     *  Distribute the negative and positive to the odd and even number        \
-     *  in the uint_*_t range.                                                 \
-     *                                                                         \
-     *  In this way, -1 can be encoded to 1 bytes since it is mapped to 1.     \
-     *                                                                         \
-     * \see https://github.com/dominictarr/signed-varint                       \
-     */                                                                        \
-                                                                               \
-    /* num = (num < 0) ? (num * -2 - 1) : (num * 2) */                         \
-    num = (num & ((uint##bits_##_t)(1) << ((bits_)-1))) ? (((~num) << 1) + 1)  \
-                                                        : (num << 1);          \
-    kvarint_encode##bits_((uint##bits_##_t)num, buf);                          \
+#define KVARINT_DECL_ENCODE_FUNC_SIGNED_(bits_)                                                    \
+  INLINE void kvarint_encode##bits_##s(int##bits_##_t num, kvarint_buf##bits_##_t *buf)            \
+  {                                                                                                \
+    /* Since -1 is (~0), ie. the maximum number of uint_*_t,                                       \
+     * it will be encoded to 10 bytes of uint64_t.                                                 \
+     *                                                                                             \
+     * However, user want the -1 also be encoded to 1 bytes like 1.                                \
+     * For other negative whose absolute value is small, also be encoded to                        \
+     * same bytes with their absolute.                                                             \
+     *                                                                                             \
+     * Algorithm:                                                                                  \
+     *  Distribute the negative and positive to the odd and even number                            \
+     *  in the uint_*_t range.                                                                     \
+     *                                                                                             \
+     *  In this way, -1 can be encoded to 1 bytes since it is mapped to 1.                         \
+     *                                                                                             \
+     * \see https://github.com/dominictarr/signed-varint                                           \
+     */                                                                                            \
+                                                                                                   \
+    /* num = (num < 0) ? (num * -2 - 1) : (num * 2) */                                             \
+    num = (num & ((uint##bits_##_t)(1) << ((bits_)-1))) ? (((~num) << 1) + 1) : (num << 1);        \
+    kvarint_encode##bits_((uint##bits_##_t)num, buf);                                              \
   }
 
 KVARINT_DECL_ENCODE_FUNC_SIGNED_(64)
